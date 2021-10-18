e4435003741ffd9c840dfcb7cdaf0bb0b4f9d220
Yetife <tifetreats21@gmail.com>
2021-10-18 23:41:09 +0100

    Added new file

---
+++ Practice/tuple.py
@@ -0,0 +1,9 @@
+days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+number = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+chars = ("a", "b", "c", "d", "e")
+
+print ("Day one", days[1], "number [1:6]", number[1:6])
+print("maximum number is: ", max(number))
+
+del chars
+print(chars)
