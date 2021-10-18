a834fea12b94c36f1eee5ce48916507ce79b8e97
Yetife <tifetreats21@gmail.com>
2021-10-18 23:40:08 +0100

    Added new file

---
+++ Practice/list.py
@@ -0,0 +1,10 @@
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+chars = ["a", "b", "c", "d", "e"]
+
+print ("Day one is", days[1], "number [1:6]", numbers[1:6])
+print ("Day two is", days[2])
+days[2] = "January"
+print ("Day two is", days[2])
+del days[2];
+print ("Day two is", days[2])
