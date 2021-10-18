75c17498ca1027b224cbb9c4f6aa824043d17e33
Yetife <tifetreats21@gmail.com>
2021-10-18 23:41:34 +0100

    Added new file

---
+++ Practice/while_loop.py
@@ -0,0 +1,13 @@
+count = 0;
+while (count < 3):
+    count2 = 0;
+    while (count2 < 3):
+        print("Current value of count: ", count, "and count2", count2)
+        count2 = count2 + 1;
+    count = count + 1;
+    
+
+var1 = "good";
+var2 = "language"
+var3 = 1;
+print("Python is a %s programming %s and number %d" %(var1, var2, var3))
