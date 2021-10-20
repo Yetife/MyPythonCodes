be4c43e8c2dd32adb5d4368add634e249226b3bc
Yetife <tifetreats21@gmail.com>
2021-10-21 00:35:16 +0100

    added file

---
+++ Practice/radius.py
@@ -0,0 +1,13 @@
+import math
+radius = int(input())
+
+area_of_circle = 3.142 * radius **2
+print(area_of_circle)
+print("circumference : " + str(2*math.pi))
+
+
+
+lower_case = input().capitalize
+print(lower_case)
+
+def addNumbers()
