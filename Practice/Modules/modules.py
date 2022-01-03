c67264e3c692722561a707fce391043b75c5542d
Yetife <tifetreats21@gmail.com>
2022-01-03 21:41:02 +0100

    Python practice

---
+++ Practice/Modules/modules.py
@@ -0,0 +1,15 @@
+import converters
+from converters import lbs_to_kg
+from find_max import find_max
+# import ecommerce.shipping
+from ecommerce.shipping import calc_shipping
+from ecommerce import shipping
+
+shipping.calc_shipping()
+calc_shipping()
+
+print(lbs_to_kg(155)) 
+
+print(converters.kg_to_lbs(70))
+
+print(find_max([5,10,20,30,50]))
