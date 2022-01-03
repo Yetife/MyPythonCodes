8f9983dbda2309392fdae93d4d091022643b95a4
Yetife <tifetreats21@gmail.com>
2022-01-03 21:41:15 +0100

    Python practice

---
+++ Practice/mystery_no.py
@@ -0,0 +1,14 @@
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+
+while guess_count < guess_limit:
+    guess = int(input('Guess a number: '))
+    guess_count += 1
+    if guess == secret_number:
+        print('You won!')
+        break
+    else:
+        print('You guessed wrong')
+else:
+    print('Sorry you failed')
