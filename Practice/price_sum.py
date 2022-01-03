73057dc88056584e5b7fb8017f20bc532e2cedf5
Yetife <tifetreats21@gmail.com>
2022-01-03 21:41:24 +0100

    Python practice

---
+++ Practice/price_sum.py
@@ -0,0 +1,15 @@
+prices = [10, 20, 30 ,40, 50]
+
+total = 0
+for price in prices:
+    total += price
+print(f'Total price of item is {total}')
+
+
+for x in range(3):
+    for y in range(5):
+        print(f'({x}, {y})') 
+
+
+
+
