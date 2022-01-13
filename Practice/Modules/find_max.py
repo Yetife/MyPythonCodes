f5c26813bcc05a700649cfa66d89319d48b369a5
Yetife <tifetreats21@gmail.com>
2022-01-13 22:42:36 +0100

    Python practice

--- Practice/Modules/find_max.py
+++ Practice/Modules/find_max.py
@@ -1,10 +1,11 @@
-def find_max(list):
+def find_max(number):
     maximum = 0
-    for i in list:
+    for i in number:
         if i > maximum:
             maximum = i
     return maximum
 
+
 print(find_max([1, 2, 3]))
 
-#package is a container for multiple modules. It is a directory or folder
+# package is a container for multiple modules. It is a directory or folder
